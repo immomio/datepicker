e59e61be5ed69943aa850be3a49958e1c75aa38a
Tobias Kopelke <kopelke@gmail.com>
2016-01-08 13:22:31 +0100

    hinzufügen eines Filters für Datumskovertierung

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,23 @@
 var module = angular.module('datepicker', []);
 $.datepicker.setDefaults($.datepicker.regional.de);
 
+// Konstante die unser Standard-Format enthält, muss natürlich in Produktion
+// anders funktionieren, um das Format der aktuellen Sprache zurückzuliefern.
+module.constant('DEFAULT_FORMAT', 'DD.MM.YYYY');
+
+module.filter('format', function(DEFAULT_FORMAT) {
+  // input erwartet ein JS-Date Objekt oder ein moment Objekt zu sein
+  return function(input, format) {
+    if(input !== null || !angular.object(input)) {
+      throw new Error('Nicht erlaubtes Datenformat für format-Filter: ' + typeof input);
+    }
+    if(!moment.isMoment(input)) {
+      input = moment(input); // auto-konvertieren von JS-Date Objekten zu moment
+    }
+    return input.format(format || DEFAULT_FORMAT);
+  };
+});
+
 module.controller('mainController', function ($scope) {
   $scope.user = {
     datePicker: moment().format('YYYY-MM-DD'),
